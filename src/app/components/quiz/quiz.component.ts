f46ee2cc4bd3d96cf2c5ae159d718eb1f0d11f7e
Arielli Szeremeta Fioravante <arielli_sf_@hotmail.com>
2023-05-15 17:07:41 -0300

    quiz rules and css created

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import quiz_questions from "../../../assets/data/quiz_questions.json"
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css']
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit{
+  
+  @Input()
+  title:string = ""
 
+  questions:any 
+  questionSelected:any 
+
+  answers:string[] = []
+  answerSelected:string = ""
+
+  questionIndex: number = 0
+  questionMaxIndex: number = 0
+
+  finished:boolean = true
+
+  constructor( ){ }
+
+  ngOnInit(): void {
+
+    if(quiz_questions){
+      this.finished = false;
+      this.title = quiz_questions.title
+
+      this.questions = quiz_questions.questions
+      this.questionSelected = this.questions [this.questionIndex]
+
+      this.questionMaxIndex = this.questions.length
+
+      console.log(this.questionIndex)
+      console.log(this.questionMaxIndex)
+    }
+     
+   }
+
+   playerChoose(value:string){
+    this.answers.push(value)
+   }
+
+   async nextStep(){
+    this.questionIndex++
+
+    if(this.questionIndex < this.questionMaxIndex){
+      this.questionSelected = this.questions[this.questionIndex]
+    } else{
+      this.finished = true
+    }
+  }
+   
 }
